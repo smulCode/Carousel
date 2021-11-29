7f59e22c810d126e0540744b6af4ce35565801e5
smulCode <67417862+smulCode@users.noreply.github.com>
2021-11-29 10:34:22 +0100

    update javascript

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,113 @@
-const track = document.querySelector('.carousel-track');
+const track = document.querySelector(".carousel-track");
 const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel-button--right');
-const previousButton = document.querySelector('.carousel-button--left');
+const nextButton = document.querySelector(".carousel-button--right");
+const previousButton = document.querySelector(".carousel-button--left");
+const dotsNav = document.querySelector(".carousel-nav");
+const dots = Array.from(dotsNav.children);
+// const backgroundTransform = document.querySelector(".active-transition");
 
-console.log(slides);
+
+// console.log(slides);
 
 // const slideSize = slides[0].getBoundingClientRect();
 // console.log(slideSize);
 // const slideWidth = slideSize.width;
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-console.log(slideWidth);
+// console.log(slideWidth);
 
-slides[0].style.left = 0;
+//arrange the slides next to one another
+// slides[0].style.left = slideWidth * 0 +  'px';
+// slides[1].style.left = slideWidth * 1 +  'px';
+// slides[2].style.left = slideWidth * 2 +  'px';
+
+// slides.forEach((slide, index) => {
+//     slide.style.left = slideWidth * index + 'px';
+// })
+
+//arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+    currentSlide.classList.remove("current-slide");
+    targetSlide.classList.add("current-slide");
+    
+  };
+  
 
 
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove("current-slide");
+  targetDot.classList.add("current-slide");
+};
+
+const hideShowArrows = (slides, previousButton, nextButton, targetIndex) => {
+
+    
+  if (targetIndex === 0) {
+    previousButton.classList.add('is-hidden');  
+    nextButton.classList.remove('is-hidden'); 
+} else if (targetIndex === slides.length - 1){
+    previousButton.classList.remove('is-hidden');
+    nextButton.classList.add('is-hidden');
+}else {
+    previousButton.classList.remove('is-hidden');
+    nextButton.classList.remove('is-hidden');
+}
+
+
+}
+
+//when i click left, move to the left;
+previousButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const previousSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const previousDot = currentDot.previousElementSibling;
+  const previousIndex = slides.findIndex(slide => slide === previousSlide) 
+
+  moveToSlide(track, currentSlide, previousSlide);
+  updateDots(currentDot, previousDot);
+  hideShowArrows(slides, previousButton, nextButton, previousIndex);
+});
+
+//when i click right, move to the right;
+nextButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide) 
+
+  moveToSlide(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
+  hideShowArrows(slides, previousButton, nextButton, nextIndex);
+});
+
+//when I click the nav indicator ,move to that slide
+
+dotsNav.addEventListener("click", (e) => {
+  const targetDot = e.target.closest("button");
+
+  if (!targetDot) return;
+
+  const currentSlide = track.querySelector(".current-slide");
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const targetIndex = dots.findIndex((dot) => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+
+
+  moveToSlide(track, currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);
+  hideShowArrows(slides, previousButton, nextButton, targetIndex);
+ 
+
+});
 
 //options
 //arrange the slides next to one another
@@ -30,6 +123,3 @@ slides[0].style.left = 0;
 //when i click left ,move slide behind other slide on the left side
 //when i click right ,move slide behind other slide on the right side
 //Expand buttons to make it touch friendly
-
-
-
